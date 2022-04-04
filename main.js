36bc3354d6e7ba1d443802709a1a236844bf5bc7
moni0012 <moni.kumari@db.com>
2022-04-04 15:38:40 +0300

    javascript challenges

---
+++ main.js
@@ -0,0 +1,51 @@
+//using if to find positive or negatib=ve number
+const number = 10;
+
+if (number % 2 === 0) {
+    console.log("number is positive");
+
+} else {
+    console.log('number is negative');
+}
+
+//using if statement to determine greatest of all 3 numbers
+
+const num1 = 30;
+const num2 = 20;
+const num3 = 10;
+
+if ((num1 >= num2) && (num2 >= num3)) {
+    console.log("num1 is greatest among all numbers");
+}
+
+
+
+//using switch for days
+
+const days = 'Tuesday';
+
+switch (days) {
+    case 'Monday':
+        console.log('Today is Monday');
+        break;
+    case 'Tuesday':
+        console.log('Only 3 more days until weekend');
+        break;
+    case 'Wednesday':
+        console.log('Today is Wednesday');
+        break;
+    case 'Thursday':
+        console.log('Today is Thursday');
+        break;
+    case 'Friday':
+        console.log('Today is Friday');
+        break;
+    case 'Saturday':
+        console.log('Today is Saturday');
+        break;
+
+    default:
+        console.log('Sorry we did not recognised the day');
+}
+
+
